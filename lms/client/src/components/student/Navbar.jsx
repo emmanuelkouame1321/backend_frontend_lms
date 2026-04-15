b882a73f8548271e9b218351bbbff8d8ad8e7cee
Emmanuel Kouame <2kgelab@gmail.com>
2026-04-15 09:28:04 +0000

    action

--- lms/client/src/components/student/Navbar.jsx
+++ lms/client/src/components/student/Navbar.jsx
@@ -3,15 +3,38 @@ import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
 import { useClerk, UserButton, useUser } from '@clerk/react'
 import { AppContext } from '../../context/AppContext'
+import { toast } from 'react-toastify'
+import axios from 'axios'
 
 const Navbar = () => {
 
-  const {navigate, isEducator} = useContext(AppContext)
+  const {navigate, isEducator, backendUrl, setIsEducator, getToken} = useContext(AppContext)
 
   const isCourseListPage = location.pathname.includes('/course-list');
 
   const {openSignIn} = useClerk()
   const {user} = useUser()
+
+  const becomeEducator = async ()=>{
+    try {
+        if(isEducator){
+            navigate('/educator')
+            return;
+        }
+        const token = await getToken()
+        const { data } = await axios.get(backendUrl + '/api/educator/update-role', {headers: {Authorization: `Bearer ${token}`}})
+
+        if(data.success){
+            setIsEducator(true)
+            toast.success(data.message)
+        }else{
+            toast.error(data.message)
+        }
+
+    } catch (error) {
+        toast.error(error.message)
+    }
+  }
 
 
   return (
@@ -27,7 +50,7 @@ const Navbar = () => {
             <div className='hidden md:flex items-center gap-5 text-gray-500'>
                 <div className='flex items-center gap-5'>
                     { user && <>
-                        <button onClick={()=> {navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button> | <Link to='/my-enrollments'>My Enrollments</Link>
+                        <button onClick={becomeEducator}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button> | <Link to='/my-enrollments'>My Enrollments</Link>
                         </>
                     }
                 </div>
@@ -40,7 +63,7 @@ const Navbar = () => {
                 <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
                     { user && 
                         <>
-                            <button onClick={()=> {navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button> | <Link to='/my-enrollments'>My Enrollments</Link>
+                            <button onClick={becomeEducator}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button> | <Link to='/my-enrollments'>My Enrollments</Link>
                         </>
                     }
                 </div>
--- lms/server/middlewares/authMiddleware.js
+++ lms/server/middlewares/authMiddleware.js
@@ -3,18 +3,18 @@ import { clerkClient, getAuth } from "@clerk/express";
 // Middleware (protect Educator Routes)
 export const protectEducator = async (req, res, next) => {
     try {
-        // ✅ Récupérer correctement l'utilisateur
+        // Récupérer correctement l'utilisateur
         const { userId } = getAuth(req);
 
-        // ✅ Vérifier si connecté
+        // Vérifier si connecté
         if (!userId) {
             return res.json({ success: false, message: "Not Authenticated" });
         }
 
-        // ✅ Récupérer les infos utilisateur
+        // Récupérer les infos utilisateur
         const user = await clerkClient.users.getUser(userId);
 
-        // ✅ Vérifier le rôle
+        // Vérifier le rôle
         if (user.publicMetadata?.role !== "educator") {
             return res.json({ success: false, message: "Unauthorized Access" });
         }
